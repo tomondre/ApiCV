730802a779fad506367bc56a3b11895221009781
tomas.ondrejka2000@gmail.com <304150@via.dk>
2021-11-27 00:06:20 +0100

    make it better

--- index.js
+++ index.js
@@ -5,26 +5,26 @@ const fs = require('fs');
 const app = express();
 
 app.get("/", (req, res) => {
-    displaySite("data/Index.json", (json) => res.json(json));
+    displaySite("data/Index.json", "Index", (json) => res.json(json));
 });
 
 app.get("/education", (req, res) => {
-    displaySite("data/MyEducation.json", (json) => res.json(json));
+    displaySite("data/MyEducation.json", "Education", (json) => res.json(json));
 });
 
 app.get("/jobs", (req, res) => {
-    displaySite("data/WorkHistory.json", (json) => res.json(json));
+    displaySite("data/WorkHistory.json","Work History", (json) => res.json(json));
 });
 
 app.get("/projects", (req, res) => {
-    displaySite("data/Projects.json", (json) => res.json(json));
+    displaySite("data/Projects.json", "Projects",(json) => res.json(json));
 });
 
 app.get("/links", (req, res) => {
-    displaySite("data/Links.json", (json) => res.json(json));
+    displaySite("data/Links.json", "Links", (json) => res.json(json));
 });
 
-function displaySite(jsonLink, callback) {
+function displaySite(jsonLink, t, callback) {
     fs.readFile("data/ApiNavigation.json", (err, navigationData)=>{
         if (err)
         {
@@ -37,9 +37,10 @@ function displaySite(jsonLink, callback) {
                 console.error(err);
                 return;
             }
+            let title = t;
             let navigation = JSON.parse(navigationData.toString())
             let content = JSON.parse(data.toString());
-            callback({content, navigation});
+            callback({title, content, navigation});
         });
     });
 }
